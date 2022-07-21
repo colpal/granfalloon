ec665fd99a3ca66417f7ff296bc180ac07d95c89
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-21 17:53:36 -0400

    Expand on the start-challenge route

--- src/routes/start-challenge.mjs
+++ src/routes/start-challenge.mjs
@@ -1,3 +1,21 @@
-export default () => ({
-  body: 'Start Challenge',
-});
+import getJSON from '../get-json.mjs';
+import attempt from '../attempt.mjs';
+
+export default async (request) => {
+  const [bodyError, body] = await attempt(getJSON(request));
+  if (bodyError) {
+    return {
+      status: 422,
+      body: 'A JSON body is required',
+    };
+  }
+
+  if (!body.publicKey) {
+    return {
+      status: 422,
+      body: 'The JSON body must include a "publicKey" key',
+    };
+  }
+
+  return { status: 200 };
+};
