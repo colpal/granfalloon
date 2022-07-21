6aed36b9fb39d8193af914da8358a27fda4b8459
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-21 12:56:08 -0400

    Add src/wrap-route.mjs fn

---
+++ src/wrap-route.mjs
@@ -0,0 +1,11 @@
+export default (fn) => async (req, res) => {
+  try {
+    const { status, body, headers } = await fn(req);
+    return res
+      .writeHead(status ?? 200, headers)
+      .end(body);
+  } catch (e) {
+    console.error(e);
+    return res.writeHead(500).end();
+  }
+};
