5ec20a45710ffa1f789b499f067197da283fe85e
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-22 13:48:04 -0400

    Connect ./routes/start-challenge.js to the router

--- src/mod.js
+++ src/mod.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env deno run --allow-net
 import { serve } from './deps.ts';
+import startChallenge from './routes/start-challenge.js'
 
 serve((request) => {
   const url = new URL(request.url);
   switch (url.pathname) {
     case '/_/start-challenge':
-      return new Response('Start Challenge');
+      return startChallenge(request);
     case '/_/complete-challenge':
       return new Response('Complete Challenge');
     default:
