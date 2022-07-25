1123fc1b1b8c978b64eb1ae0d9c1f2117cf5e423
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-25 16:23:26 -0400

    Add std/flags@0.149.0 to the dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,3 @@
 export { serve } from "https://deno.land/std@0.149.0/http/mod.ts";
 export { crypto } from "https://deno.land/std@0.149.0/crypto/mod.ts";
+export { parse } from "https://deno.land/std@0.149.0/flags/mod.ts";
