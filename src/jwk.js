9711b8001230ee5f9a97be765fc26f7f24bda307
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-25 17:22:00 -0400

    Add draft fn for computing thumbprint of RSA JWK

--- src/jwk.js
+++ src/jwk.js
@@ -1,7 +1,21 @@
+import { crypto } from './deps.ts';
+
+async function thumbprintRSA(jwk) {
+  const container = {};
+  ["e", "kty", "n"].forEach(k => {
+    container[k] = jwk[k];
+  });
+  const json = JSON.stringify(container);
+  const textEncoder = new TextEncoder();
+  const encoded = textEncoder.encode(json);
+  const hash = await crypto.subtle.digest('SHA-256', encoded);
+  return '';
+}
+
 export function thumbprint(k) {
   switch (k.kty) {
     case "RSA":
-      return 0;
+      return thumbprintRSA(k);
     default:
       throw new Error(`Unsupported kty: "${k.kty}"`);
   }
