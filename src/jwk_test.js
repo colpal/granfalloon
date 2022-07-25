55426f892e0fee842b1536c74efb18fbcd1bb477
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-25 17:12:59 -0400

    Change thumbprint RSA test to expect async functions

--- src/jwk_test.js
+++ src/jwk_test.js
@@ -1,7 +1,7 @@
 import { assertEquals } from "./deps.ts";
 import { thumbprint } from "./jwk.js";
 
-Deno.test("thumbprint RSA", () => {
+Deno.test("thumbprint RSA", async () => {
   const publicKey = {
     kty: "RSA",
     n: "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
@@ -10,6 +10,6 @@ Deno.test("thumbprint RSA", () => {
     kid: "2011-04-29",
   };
   const expected = "NzbLsXh8uDCcd-6MNwXF4W_7noWXFZAfHkxZsRGC9Xs";
-  const actual = thumbprint(publicKey);
+  const actual = await thumbprint(publicKey);
   assertEquals(actual, expected);
 });
