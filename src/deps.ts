466187d46ff5c7afb12470fddeeeaffa1163a6e8
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-25 16:57:34 -0400

    Add std/testing/asserts@0.149.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,4 @@
 export { serve } from "https://deno.land/std@0.149.0/http/mod.ts";
 export { crypto } from "https://deno.land/std@0.149.0/crypto/mod.ts";
 export { parse } from "https://deno.land/std@0.149.0/flags/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
