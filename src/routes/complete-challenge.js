31a19ee7f7e5699c38889ebc33054bbd5bdffda2
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-26 17:50:18 -0400

    Partially implement complete-challenge route

--- src/routes/complete-challenge.js
+++ src/routes/complete-challenge.js
@@ -1,1 +1,29 @@
-export default () => new Response('Complete Challenge');
+import attempt from "../attempt.js";
+
+export default async (request, { store }) => {
+  const [bodyError, { nonce, answer }] = await attempt(request.json());
+  if (bodyError) {
+    return new Response("A JSON body is required", { status: 400 });
+  }
+
+  if (!nonce) {
+    return new Response("The JSON body must include a 'nonce' key", {
+      status: 400,
+    });
+  }
+
+  if (!answer) {
+    return new Response("The JSON body must include an 'answer' key", {
+      status: 400,
+    });
+  }
+
+  const [expectedGetError, expected] = await attempt(store.get(nonce));
+  if (expectedGetError) {
+    return new Response("Could not retrieve answer from the store", {
+      status: 500,
+    });
+  }
+
+  return new Response("Complete Challenge");
+};
