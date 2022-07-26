b310287a0934ffd38d6617da569ce8a0f1d11ea6
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-26 13:53:57 -0400

    Change load to return a tuple of [thumbprint, profile]

--- src/profile.js
+++ src/profile.js
@@ -1,3 +1,6 @@
+import { thumbprint } from './jwk.js';
+
 export async function load(path) {
-  return JSON.parse(await Deno.readTextFile(path));
+  const profile = JSON.parse(await Deno.readTextFile(path));
+  return [await thumbprint(profile.publicKey), profile];
 }
