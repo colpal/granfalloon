8ca134484cd85d91e12e07911b7f80481b281cbf
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-27 16:22:37 -0400

    Implement first draft of src/routes/pass-through

--- src/routes/pass-through.js
+++ src/routes/pass-through.js
@@ -1,1 +1,20 @@
-export default () => new Response(null, { status: 404 });
+export default async (request, { store, profiles }) => {
+  const authorization = request.headers.get("Authorization");
+  if (!authorization) return new Response(null, { status: 401 });
+
+  const matches = authorization.match(/token (.*)/);
+  if (!matches) return new Response(null, { status: 401 });
+  const [_, session] = matches;
+
+  const [getKIDError, kid] = await attempt(store.get(session));
+  if (getKIDError) return new Response(null, { status: 500 });
+  if (!kid) return new Response(null, { status: 401 });
+
+  const profile = profiles[kid];
+  if (!profile) return new Response(null, { status: 500 });
+
+  const isAllowed = profiles.allow.some(() => false);
+  if (!isAllowed) return new Response(null, { status: 403 });
+
+  return new Response(null, { status: 200 });
+};
