d7db87c0f0c85246e86666ba028f84dd3020dd64
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-28 12:24:51 -0400

    Simplify start-challenge test

--- src/routes/start-challenge.test.js
+++ src/routes/start-challenge.test.js
@@ -3,12 +3,14 @@ import startChallenge from "./start-challenge.js";
 import * as InMemoryStore from "../store/in-memory.js";
 import load from "../profiles/load.js";
 
-const profilePath = "./test/profiles/example.json";
+const profiles = Object.fromEntries([
+  await load("./test/profiles/example.json"),
+]);
 
 Deno.test("empty body", async () => {
-  const store = InMemoryStore.create();
-  const profiles = Object.fromEntries([await load(profilePath)]);
-  const request = new Request("http://localhost");
-  const { status } = await startChallenge(request, { store, profiles });
+  const { status } = await startChallenge(
+    new Request("http://localhost/_/start-challenge"),
+    { profiles, store: InMemoryStore.create() },
+  )
   assertNotEquals(status, 200);
 });
