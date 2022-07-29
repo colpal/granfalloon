4fae70f9786cece9ff02928775b071ed2bf7152b
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-29 14:29:51 -0400

    Export std/encoding/base64.decode as base64Decode

--- src/deps.ts
+++ src/deps.ts
@@ -6,5 +6,8 @@ export {
   assertNotEquals,
 } from "https://deno.land/std@0.149.0/testing/asserts.ts";
 export { encode as base64urlEncode } from "https://deno.land/std@0.149.0/encoding/base64url.ts";
-export { encode as base64Encode } from "https://deno.land/std@0.149.0/encoding/base64.ts";
+export {
+  decode as base64Decode,
+  encode as base64Encode,
+} from "https://deno.land/std@0.149.0/encoding/base64.ts";
 export { globToRegExp } from "https://deno.land/std@0.149.0/path/glob.ts";
