b13783986a12430f391800c0587f4f32666ac88b
Nicholas Farley <nicholasafarley@gmail.com>
2022-07-29 15:05:27 -0400

    Pull penultimate token from GRANFALLOON_TOKEN env variable

--- src/mod.js
+++ src/mod.js
@@ -8,4 +8,5 @@ serve(router({
   store: InMemoryStore.create(),
   profiles: Object.fromEntries(await Promise.all(Deno.args.map(load))),
   target: "https://jsonplaceholder.typicode.com",
+  token: Deno.env.get("GRANFALLOON_TOKEN"),
 }));
