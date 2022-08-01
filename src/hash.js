5f2c8a757583f3f0c069ee44ae85d922cf3a1dda
Nicholas Farley <nicholasafarley@gmail.com>
2022-08-01 19:07:22 -0400

    Add src/hash.js module

---
+++ src/hash.js
@@ -0,0 +1,7 @@
+import { crypto } from "./deps.ts";
+
+export default async (algorithm, message) => {
+  const encoded = new TextEncoder().encode(message);
+  const ab = await crypto.subtle.digest(algorithm, encoded);
+  return Array.from(new Uint8Array(ab)).map(b => b.toString(16)).join('');
+};
