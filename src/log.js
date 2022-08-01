600d9a21546649b54e16524653a73d903d0caa65
Nicholas Farley <nicholasafarley@gmail.com>
2022-08-01 18:01:55 -0400

    Use multiple exports for src/log.js module

--- src/log.js
+++ src/log.js
@@ -1,10 +1,9 @@
-export default {
-  info(x) {
-    console.info(x);
-    return x;
-  },
-  error(x) {
-    console.error(x);
-    return x;
-  },
+export const info = (x) => {
+  console.info(x);
+  return x;
+};
+
+export const error = (x) => {
+  console.error(x);
+  return x;
 };
