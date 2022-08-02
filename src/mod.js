f4bc18565c34904dabc4d539d5ea8698b1ef921a
Nicholas Farley <nicholasafarley@gmail.com>
2022-08-02 17:41:14 -0400

    Load target and profile information from CLI arguments

--- src/mod.js
+++ src/mod.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env deno run --allow-net --allow-read --allow-env
 import { serve } from "./deps.ts";
-import load from "./profiles/load.js";
+import loadDir from "./profiles/load-dir.js";
 import * as InMemoryStore from "./store/in-memory.js";
 import router from "./router.js";
 import * as log from "./log.js";
+import parseFlags from "./flags.js";
+
+const { target, profileDir } = await parseFlags(Deno.args);
 
 serve(router({
   log,
+  target,
   store: InMemoryStore.create(),
-  profiles: Object.fromEntries(await Promise.all(Deno.args.map(load))),
-  target: "https://jsonplaceholder.typicode.com",
+  profiles: await loadDir(profileDir),
   token: Deno.env.get("GRANFALLOON_TOKEN"),
 }));
