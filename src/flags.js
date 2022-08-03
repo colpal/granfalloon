f298d67401a8d53075010c79e9786a0690f68256
Nicholas Farley <nicholasafarley@gmail.com>
2022-08-03 13:43:18 -0400

    Add support for configuring store (in-memory only, for now)

--- src/flags.js
+++ src/flags.js
@@ -11,11 +11,18 @@ const transformers = {
     if (!stat.isDirectory) throw new Error(`'${v}' is not a directory`);
     return v;
   },
+  store({ store: v }) {
+    if (v !== "in-memory") throw new Error(`unsupported store '${v}'`);
+    return v;
+  },
 };
 
 export default async (args) => {
   const parsedArgs = parse(args, {
     string: Object.keys(transformers),
+    default: {
+      store: "in-memory",
+    },
     unknown(_, k) {
       throw new Error(`unrecognized argument "${k}"`);
     },
