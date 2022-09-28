c12f871d11314e74b28bc38b9a8e489eccf663e0
Nicholas Farley <nicholas_farley@colpal.com>
2022-09-28 13:49:25 -0400

    Add src/url.shift fn

--- src/url.js
+++ src/url.js
@@ -6,3 +6,10 @@ export const isMatch = (allows, request) => {
     return request.method === method && globToRegExp(glob).test(pathname);
   });
 };
+
+export const shift = (remote, request, token) => {
+  const headers = new Headers(request.headers);
+  headers.set("Authorization", `token ${token}`);
+  const url = new URL((new URL(request.url)).pathname, remote);
+  return new Request(url, { ...request, headers });
+};
