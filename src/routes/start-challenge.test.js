c6671c339fc58c3709cb378d94dedb5e0db397e1
Nicholas Farley <nicholas_farley@colpal.com>
2022-10-21 17:06:41 -0400

    Create a "store error" test for Ed25519

--- src/routes/start-challenge.test.js
+++ src/routes/start-challenge.test.js
@@ -24,6 +24,26 @@ const unknownPublicKey = (publicKey) => async () => {
   assertEquals(floorHundred(status), 400);
 };
 
+const storeError = (publicKey) => async () => {
+  const { status } = await startChallenge(
+    new Request(url, {
+      method: "POST",
+      body: JSON.stringify({ publicKey }),
+    }),
+    {
+      log,
+      profiles,
+      store: {
+        // deno-lint-ignore require-await
+        async set() {
+          throw new Error();
+        },
+      },
+    },
+  );
+  assertEquals(floorHundred(status), 500);
+};
+
 Deno.test("empty body", async () => {
   const { status } = await startChallenge(
     new Request(url),
@@ -54,22 +74,5 @@ Deno.test("empty public key", async () => {
 Deno.test("RSA: unknown public key", unknownPublicKey(rsaProfile.publicKey));
 Deno.test("Ed25519: unknown public key", unknownPublicKey(ed25519Profile.publicKey));
 
-Deno.test("store error", async () => {
-  const { status } = await startChallenge(
-    new Request(url, {
-      method: "POST",
-      body: JSON.stringify({ publicKey: rsaProfile.publicKey }),
-    }),
-    {
-      log,
-      profiles,
-      store: {
-        // deno-lint-ignore require-await
-        async set() {
-          throw new Error();
-        },
-      },
-    },
-  );
-  assertEquals(floorHundred(status), 500);
-});
+Deno.test("RSA: store error", storeError(rsaProfile.publicKey));
+Deno.test("Ed25519: store error", storeError(ed25519Profile.publicKey));
