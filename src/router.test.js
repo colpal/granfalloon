2d7c80df04b984ff00e5fe6942697a17bb2add72
Nicholas Farley <nicholas_farley@colpal.com>
2022-10-21 16:44:37 -0400

    Make router.test.js end-to-end test reusable

--- src/router.test.js
+++ src/router.test.js
@@ -5,58 +5,63 @@ import * as InMemoryStore from "./store/in-memory.js";
 import load from "./profiles/load.js";
 import Router from "./router.js";
 
+const endToEnd = (profilePath, privatePath) => async() => {
+  const remote = "https://api.github.com";
+  const pathname = "user";
+  const token = Deno.env.get("GRANFALLOON_TOKEN");
+  const log = { info: (x) => x, error: (x) => x };
+
+  const [kid, profile] = await load(profilePath);
+  const router = Router({
+    log,
+    remote,
+    token,
+    store: InMemoryStore.create(),
+    profiles: Object.fromEntries([[kid, profile]]),
+  });
+
+  const start = await router(
+    new Request("http://localhost/_/start-challenge", {
+      method: "POST",
+      body: JSON.stringify({ publicKey: profile.publicKey }),
+    }),
+  );
+
+  const { data: { nonce, challenge } } = await start.json();
+  const answer = await Deno
+    .readTextFile(privatePath)
+    .then(JSON.parse)
+    .then(toDecryptionKey)
+    .then((key) => decrypt(key, challenge));
+  const complete = await router(
+    new Request("http://localhost/_/complete-challenge", {
+      method: "POST",
+      body: JSON.stringify({ nonce, answer }),
+    }),
+  );
+
+  const { data: { session } } = await complete.json();
+  const actual = await router(
+    new Request(`http://localhost/${pathname}`, {
+      headers: { Authorization: `token ${session}` },
+    }),
+  );
+
+  const expected = await fetch(`${remote}/${pathname}`, {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  });
+
+  assertEquals(await actual.json(), await expected.json());
+}
+
 Deno.test({
   name: "end-to-end",
   sanitizeOps: false,
   sanitizeResources: false,
-  fn: async () => {
-    const remote = "https://api.github.com";
-    const pathname = "user";
-    const token = Deno.env.get("GRANFALLOON_TOKEN");
-    const log = { info: (x) => x, error: (x) => x };
-
-    const [kid, profile] = await load("./test/profiles/example-rsa.json");
-    const router = Router({
-      log,
-      remote,
-      token,
-      store: InMemoryStore.create(),
-      profiles: Object.fromEntries([[kid, profile]]),
-    });
-
-    const start = await router(
-      new Request("http://localhost/_/start-challenge", {
-        method: "POST",
-        body: JSON.stringify({ publicKey: profile.publicKey }),
-      }),
-    );
-
-    const { data: { nonce, challenge } } = await start.json();
-    const answer = await Deno
-      .readTextFile("./test/profiles/example-rsa.json.private")
-      .then(JSON.parse)
-      .then(toDecryptionKey)
-      .then((key) => decrypt(key, challenge));
-    const complete = await router(
-      new Request("http://localhost/_/complete-challenge", {
-        method: "POST",
-        body: JSON.stringify({ nonce, answer }),
-      }),
-    );
-
-    const { data: { session } } = await complete.json();
-    const actual = await router(
-      new Request(`http://localhost/${pathname}`, {
-        headers: { Authorization: `token ${session}` },
-      }),
-    );
-
-    const expected = await fetch(`${remote}/${pathname}`, {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-    });
-
-    assertEquals(await actual.json(), await expected.json());
-  },
+  fn: endToEnd(
+    "./test/profiles/example-rsa.json",
+    "./test/profiles/example-rsa.json.private",
+  ),
 });
