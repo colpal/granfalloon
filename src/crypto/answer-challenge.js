2f0db1390433cd018490a1e2162ed1a83c181a51
Nicholas Farley <nicholas_farley@colpal.com>
2022-10-24 14:01:04 -0400

    Fix answer-challenge.js to expect UTF-8 text when signing

--- src/crypto/answer-challenge.js
+++ src/crypto/answer-challenge.js
@@ -1,21 +1,24 @@
 import { base64Decode } from "../deps.ts";
 
-export default async (key, b64) => {
-  let fn;
+export default async (key, challenge) => {
   switch (true) {
     case key.usages.includes("sign"):
-      fn = crypto.subtle.sign.bind(crypto.subtle);
-      break;
+      return new TextDecoder().decode(
+        await crypto.subtle.sign(
+          key.algorithm.name,
+          key,
+          new TextEncoder().encode(challenge),
+        ),
+      );
     case key.usages.includes("decrypt"):
-      fn = crypto.subtle.decrypt.bind(crypto.subtle);
-      break;
+      return new TextDecoder().decode(
+        await crypto.subtle.decrypt(
+          key.algorithm.name,
+          key,
+          base64Decode(challenge),
+        ),
+      );
     default:
       throw new Error("The supplied key can not be used to answer a challenge");
   }
-  const ab = await fn(
-    key.algorithm.name,
-    key,
-    base64Decode(b64),
-  );
-  return new TextDecoder().decode(ab);
 };
