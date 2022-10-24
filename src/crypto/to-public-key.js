88426604b1f3ebec183fe1fb1bde1372d90cd7f2
Nicholas Farley <nicholas_farley@colpal.com>
2022-10-24 11:49:10 -0400

    Update src/crypto/to-public-key.js to handle Ed25519 keys

--- src/crypto/to-public-key.js
+++ src/crypto/to-public-key.js
@@ -1,9 +1,32 @@
 export default (jwk) => {
-  return crypto.subtle.importKey(
-    "jwk",
-    jwk,
-    { name: "RSA-OAEP", hash: "SHA-256" },
-    false,
-    ["encrypt"],
-  );
+  const partial = (algorithm, usages) =>
+    crypto.subtle.importKey(
+      "jwk",
+      jwk,
+      algorithm,
+      false,
+      usages,
+    );
+  switch (jwk.kty) {
+    case "RSA":
+      switch (jwk.alg) {
+        case "RSA-OAEP-256":
+          return partial({ name: "RSA-OAEP", hash: "SHA-256" }, ["encrypt"]);
+        case "RSA-OAEP-384":
+          return partial({ name: "RSA-OAEP", hash: "SHA-384" }, ["encrypt"]);
+        case "RSA-OAEP-512":
+          return partial({ name: "RSA-OAEP", hash: "SHA-512" }, ["encrypt"]);
+        default:
+          throw new Error(`The '${jwk.alg}' algorithm isn't supported`);
+      }
+    case "OKP":
+      switch (jwk.crv) {
+        case "Ed25519":
+          return partial("Ed25519", ["verify"]);
+        default:
+          throw new Error(`The '${jwk.crv}' curve isn't supported`);
+      }
+    default:
+      throw new Error(`The '${jwk.kty}' key type isn't supported`);
+  }
 };
