3accd5e99ee40d9b1fc7b6f8d6af915250259f92
Nicholas Farley <nicholas_farley@colpal.com>
2023-01-27 15:08:11 -0500

    Add first "leg" of authentication to Python client example

--- examples/python-client/main.py
+++ examples/python-client/main.py
@@ -1,5 +1,21 @@
+#!/usr/bin/env python
+import requests
+
+
+GRANFALLOON_URL = "http://localhost:8000"
+PUBLIC_KEY = {
+    "kty": "OKP",
+    "crv": "Ed25519",
+    "x": "0_Wi5E-xXujsb_rrZ5NbDHmdji2I-ix6XIzim7b4DN8",
+}
+
+
 def main():
-    pass
+    r = requests.post(
+        f"{GRANFALLOON_URL}/_/start-challenge",
+        json={"publicKey": PUBLIC_KEY},
+    )
+    print(r.text)
 
 
 if __name__ == "__main__":
