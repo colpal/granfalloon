2507263da2a5decf24bd014a4bfc554cc0c162fd
Nicholas Farley <nicholas_farley@colpal.com>
2023-01-27 10:53:03 -0500

    Upgrade std/path/glob to 0.174.0

--- src/deps.ts
+++ src/deps.ts
@@ -9,6 +9,6 @@ export {
   decode as base64Decode,
   encode as base64Encode,
 } from "https://deno.land/std@0.174.0/encoding/base64.ts";
-export { globToRegExp } from "https://deno.land/std@0.149.0/path/glob.ts";
+export { globToRegExp } from "https://deno.land/std@0.174.0/path/glob.ts";
 export { join } from "https://deno.land/std@0.149.0/path/mod.ts";
 export { connect } from "https://deno.land/x/redis@v0.26.0/mod.ts";
